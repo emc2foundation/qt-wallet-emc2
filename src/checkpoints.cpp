c81dbb619ecc690dc41273b5a1bc43eb48ee33b0
HashUnlimited <hashrate@pm-tech.at>
2017-05-12 21:30:33 +0300

    Correct Timestamp last Checkpoint

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -44,7 +44,7 @@ namespace Checkpoints
         
     static const CCheckpointData data = {
        &mapCheckpoints,
-          1494147472, // * UNIX timestamp of last checkpoint block
+          1494133072, // * UNIX timestamp of last checkpoint block
           2147811,    // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
           2000     // * estimated number of transactions per day after checkpoint
